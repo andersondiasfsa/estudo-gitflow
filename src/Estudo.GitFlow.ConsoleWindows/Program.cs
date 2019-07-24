1fde257457f5111041284efc0280c945d88143c0
Anderson Dias <andersondiasfsa@gmail.com>
2019-07-23 22:26:06 -0300

    criacao metodo 2

--- src/Estudo.GitFlow.ConsoleWindows/Program.cs
+++ src/Estudo.GitFlow.ConsoleWindows/Program.cs
@@ -6,8 +6,14 @@ namespace Estudo.GitFlow.ConsoleWindows
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("SPRINT2_CRIACAO_PROJETO");
+            Console.WriteLine("SPRINT2_CRIACAO_METODO_2");
+            metodo2();
+
             Console.ReadKey();
+        }
+        static void metodo2()
+        {
+            Console.WriteLine("metodo2");
         }
     }
 }
