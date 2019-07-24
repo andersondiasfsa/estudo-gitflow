89fe22df8c9eb7dfff15ae3b505c1fc07f7684af
Anderson Dias <andersondiasfsa@gmail.com>
2019-07-23 22:16:04 -0300

    Criando projeto SPRINT2_CRIACAO_PROJETO

---
+++ src/Estudo.GitFlow.ConsoleWindows/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Estudo.GitFlow.ConsoleWindows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SPRINT2_CRIACAO_PROJETO");
+            Console.ReadKey();
+        }
+    }
+}
