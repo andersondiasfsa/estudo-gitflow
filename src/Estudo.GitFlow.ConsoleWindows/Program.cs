bb5c2afaef89a1565bc9cebe79d5d5f25676753c
Anderson Dias <andersondiasfsa@gmail.com>
2019-07-23 22:24:11 -0300

    adicao url

--- src/Estudo.GitFlow.ConsoleWindows/Program.cs
+++ src/Estudo.GitFlow.ConsoleWindows/Program.cs
@@ -6,7 +6,8 @@ namespace Estudo.GitFlow.ConsoleWindows
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("SPRINT2_CRIACAO_PROJETO");
+            //https://blog.ateliedocodigo.com.br/fluxo-de-versionamento-de-software-com-git-flow-b9f5195c679e
+            Console.WriteLine("SPRINT2_CRIACAO_METODO");
             Console.ReadKey();
         }
     }
